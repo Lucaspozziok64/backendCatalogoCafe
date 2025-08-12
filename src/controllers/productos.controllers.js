84b151f0dd1b86ddde539ba897f739d6e4d64cd4
Lucasfi <lukafigueroa64@gmail.com>
2025-08-11 21:17:24 -0300

    Agregamos las funciones vacias para leer Productos y crear Productos

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,7 +1,14 @@
 export const test = (req, res) => {
-    res.status(200);
-    res.send('Primera prueba desde el backend');
-}
-//agregar funcion para crear Producto
+  res.status(200);
+  res.send("Primera prueba desde el backend");
+};
 
+export const leerProductos = (req, res) => {
+
+};
+
+//agregar funcion para crear Producto
+export const crearProducto = (req, res) => {
+    
+}
 //agregar funccion para editar

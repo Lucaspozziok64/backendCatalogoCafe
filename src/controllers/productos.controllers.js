5853b03ba0b6f6742143082e3746719f0451125a
Lucasfi <lukafigueroa64@gmail.com>
2025-08-13 20:41:28 -0300

    podemos ver los datos que llegan desde la solicitud POST de prueba con POSTMAN

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -3,12 +3,18 @@ export const test = (req, res) => {
   res.send("Primera prueba desde el backend");
 };
 
-export const leerProductos = (req, res) => {
-
-};
+export const leerProductos = (req, res) => {};
 
 //agregar funcion para crear Producto
 export const crearProducto = (req, res) => {
-    
-}
+  try {
+    // recibir el objeto que tengo que agregar a la BD
+    console.log(req.body)
+    // Validar los datos del objeto
+    // guardar el objeto en la base de datos
+    // Enviar Respuesta
+  } catch (error) {
+    console.error(error);
+  }
+};
 //agregar funccion para editar

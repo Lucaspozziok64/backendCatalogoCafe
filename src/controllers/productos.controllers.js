1d5b48000534d80d8d909bca316edb14e8a59d88
Lucasfi <lukafigueroa64@gmail.com>
2025-08-18 20:26:06 -0300

    Agregamos la funcion lleer productos para devolver todos los productos

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -5,13 +5,26 @@ export const test = (req, res) => {
   res.send("Primera prueba desde el backend");
 };
 
-export const leerProductos = (req, res) => {};
+export const leerProductos = async (req, res) => {
+  try {
+
+    // 1- Buscar todos los productos en la BD
+    const listaProductos = await Producto.find()
+
+    // 2- Enviar la respuesta al front
+    res.status(200).json(listaProductos)
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ mensaje: 'Error al leer los productos' })
+  }
+};
 
 //agregar funcion para crear Producto
 export const crearProducto = async (req, res) => {
   try {
     // recibir el objeto que tengo que agregar a la BD
-    // Validar los datos del objeto
+    // Validar los datos del 0
     // guardar el objeto en la base de datos
     const nuevoProducto = new Producto(req.body)
     await nuevoProducto.save()

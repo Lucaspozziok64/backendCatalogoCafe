5b8f81dd16c0d7092e2d614df81b06a7afad3cd4
Lucasfi <lukafigueroa64@gmail.com>
2025-08-19 13:01:26 -0300

    Agregamos logica para editar producto por id

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -68,3 +68,18 @@ export const borrarProductoPorId = async (req, res) => {
 }
 
 //agregar funccion para editar
+
+export const editarProductoPorId = async (req, res) => {
+  try {
+    // 1- Buscar el producto por id y luego borrar
+    const productoModificado = await Producto.findByIdAndUpdate(req.params.id, req.body)
+    if(!productoModificado) {
+      return res.status(404).json({ mensaje: 'Producto no encontrado' })
+    }
+    // 2- Responer al front
+    res.status(200).json({ mensaje: 'Producto actualizado exitosamente' })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ mensaje: 'Error al editar el producto' })
+  }
+}

5f598bf58a3d0cdc68f9e4da82e9165b8f15f222
Lucasfi <lukafigueroa64@gmail.com>
2025-09-02 21:28:59 -0300

    agregue validaciones al campo nombreProducto, precio y imagen

--- src/middleware/validarProducto.js
+++ src/middleware/validarProducto.js
@@ -2,9 +2,31 @@ import { body } from "express-validator";
 import resultadoValidacion from "./resultadoValidacion.js";
 
 const validacionProducto = [
-  body('nombreProducto').notEmpty().withMessage('El nombre del producto es obligatorio'),
-  body('precio').notEmpty(),
-  (req, res, next) => resultadoValidacion(req, res, next) 
-]
+  body("nombreProducto")
+    .notEmpty()
+    .withMessage("El nombre del producto es obligatorio")
+    .isLength({ min: 2, max: 100 })
+    .withMessage("El nombre del producto debe tener entre 2 y 100 caracteres"),
+  body("precio")
+    .notEmpty()
+    .withMessage("El precio es un dato obligatorio")
+    .isNumeric()
+    .withMessage("El precio debe ser un numero")
+    .custom((valor) => {
+      if (valor >= 50 && valor <= 100000) {
+        return true;
+      } else {
+        throw new Error("El precio debe estar entre 50 y 100000");
+      }
+    }),
+  body("imagen")
+    .notEmpty()
+    .withMessage("La imagen es un dato obligatorio")
+    .matches(/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png|webp)/)
+    .withMessage(
+      "La imagen debe ser una URL valida y debe terminar en .jpg, .jpeg, .png o .wep"
+    ),
+  (req, res, next) => resultadoValidacion(req, res, next),
+];
 
 export default validacionProducto;
